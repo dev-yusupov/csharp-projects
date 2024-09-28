a83dc24ef41c0de309dea17bf925451b93fa5c8d
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-28 15:26:50 +0200

    Simple array creation

---
+++ Arrays/SimpleArray/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] cars = { "bmw", "mersedes", "volvo", "ford" }; // Initialize a new array containing string
+
+// Create a for loop with elements of the 'cars' array
+for (int index = 0; index < cars.Length; index++)
+{
+    Console.WriteLine($"Car Name: {cars[index]}");
+}
