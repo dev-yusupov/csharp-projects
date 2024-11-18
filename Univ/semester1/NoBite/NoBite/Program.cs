80c64a77f28004188f94b3afbd2ac464fa2e0b83
Bobur Yusupov <bobur.yu06@mail.ru>
2024-11-19 00:45:14 +0100

    nobite problem solution

---
+++ Univ/semester1/NoBite/NoBite/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NoBite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            int[] f = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                f[i] = int.Parse(Console.ReadLine()!);
+            }
+
+            bool foundZero = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (f[i] == 0)
+                {
+                    Console.WriteLine(i + 1);
+                    foundZero = true;
+                    break;
+                }
+            }
+
+            if (!foundZero)
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
