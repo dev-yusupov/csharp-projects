bd1ffc37621497bd39a408bb0c5ae0f0664aa545
Bobur Yusupov <bobur.yu06@mail.ru>
2024-11-19 00:24:42 +0100

    stormy days

---
+++ Univ/semester1/StormyDays/StormyDays/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StormyDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            int stormyDays = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int windSpeed = int.Parse(Console.ReadLine()!);
+                if (windSpeed < 100 && windSpeed > 0)
+                {
+                    stormyDays++;
+                }
+            }
+
+            Console.WriteLine(stormyDays);
+        }
+    }
+}
